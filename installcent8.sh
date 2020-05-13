1bcfb7d07675292262436174322ff7152e65f103
wiesejt <quickrunner1988@gmail.com>
2020-05-12 22:05:42 -0500

    Added logic to install script

--- installcent8.sh
+++ installcent8.sh
@@ -1,7 +1,18 @@
-#!/usr/bin/bash
-# This can be dropped on fresh server to start installs
+#!/bin/bash
+
+# ref: https://askubuntu.com/a/30157/8698
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
 
 dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
 dnf install -y ansible
-sudo -u ec2-user ansible-galaxy install -r ./requirements.yaml --force
-sudo -u ec2-user ansible-playbook ./cent8setup.yaml
+sudo -u "$real_user" ansible-galaxy install -r ./requirements.yaml --force
+sudo -u "$real_user" ansible-playbook ./cent8setup.yaml
